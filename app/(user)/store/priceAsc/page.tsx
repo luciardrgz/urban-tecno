735676c3b6dd6e7a6fc7a32af93b1d8d336f68e0
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-01-17 19:37:54 -0300

    Pages for product filters

---
+++ app/(user)/store/priceAsc/page.tsx
@@ -0,0 +1,18 @@
+import { groq } from "next-sanity";
+import { client } from "../../../../lib/sanity.client";
+import Store from "../../../../components/Store";
+
+const query = groq`
+*[_type == 'product']{
+  ...,
+  name,
+  category->{name}
+} | order(price asc)
+`;
+
+export const revalidate = 30; // revalidate this page every 60 seconds
+
+export default async function StoreList() {
+  const products = await client.fetch(query);
+  return <Store products={products} />;
+}
