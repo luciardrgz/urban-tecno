f8bb6dfe3e2a4f9d465f2647a5b062af6fcbbd7a
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-01-22 18:00:13 -0300

    CORS permissions for Sanity + Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withCSS = require("@zeit/next-css");
-
-module.exports = withCSS({});
-
 module.exports = {
   reactStrictMode: true,
   experimental: {
--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,7 +1,9 @@
 import { defineCliConfig } from "sanity/cli";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+let projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string;
+let dataset = process.env.NEXT_PUBLIC_SANITY_DATASET as string;
+
+console.log(projectId);
 
 export default defineCliConfig({
   api: {
