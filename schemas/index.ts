3cc45061ca7d9a09b9cc60bdecae750d71d963c2
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-02-13 15:53:45 -0300

    RichText for Studio

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,5 +3,13 @@ import category from "./category";
 import brand from "./brand";
 import origin from "./origin";
 import color from "./color";
+import blockContent from "./blockContent";
 
-export const schemaTypes = [product, category, brand, origin, color];
+export const schemaTypes = [
+  product,
+  category,
+  brand,
+  origin,
+  color,
+  blockContent,
+];
--- schemas/product.ts
+++ schemas/product.ts
@@ -63,7 +63,7 @@ export default defineType({
     defineField({
       name: "info",
       title: "Especificaciones",
-      type: "text",
+      type: "blockContent",
     }),
     defineField({
       name: "colors",
