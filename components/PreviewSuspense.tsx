3612761f10e2849698fcd34e1c4d5a3c715bc946
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-05-30 20:29:50 -0300

    Deleted all files for store preview

--- components/PreviewSuspense.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-
-// Once rollup supports 'use client' module directives then 'next-sanity' will include them and this re-export will not longer be necessary
-export { PreviewSuspense as default } from "next-sanity/preview";
