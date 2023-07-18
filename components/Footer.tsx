c2e380b36838c75c01583a418ef1f324711b20a9
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-07-17 22:09:34 -0300

    Minor view fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,10 @@ import { faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
-    <footer className="text-center bg-[#1b1b1b] mb-0 w-full h-20">
+    <footer className="bottom-0 text-center bg-[#1b1b1b] mb-0 w-full h-20">
       <div className="flex justify-center text-center gap-2">
         <a
-          href="https://www.instagram.com/full.tecno.mdq/"
+          href="https://www.instagram.com/urbantecno.mdq/"
           className="text-[#666666] hover:text-[#b4a07c] mt-4 transition"
         >
           <FontAwesomeIcon
@@ -16,14 +16,14 @@ function Footer() {
           />
         </a>
         <a
-          href="https://wa.me/5492236020937"
+          href="https://wa.me/5492236228659"
           className="text-[#666666] hover:text-[#b4a07c] mt-4 transition"
         >
           <FontAwesomeIcon icon={faWhatsapp} width={"1.5em"} height={"1.5em"} />
         </a>
       </div>
       <div>
-        <a href="https://www.linkedin.com/in/luciardrgz/" title="LinkedIn del desarrollador" target="_blank" rel="noreferrer" className="mt- text-[#666666] hover:no-underline hover:text-white transition">
+        <a href="https://www.linkedin.com/in/luciardrgz/" title="LinkedIn del desarrollador" target="_blank" rel="noreferrer" className="mt-2 text-[#666666] hover:no-underline hover:text-white transition">
             luciardrgz | 2023
           </a>
       </div>
